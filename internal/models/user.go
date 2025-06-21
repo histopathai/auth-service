f8f1874e99d77553bd9e477546a96b3e39e8e2fd
yasintuncerr <yasiruncerr@gmail.com>
2025-06-21 14:48:17 +0300

    Add user model and request types for user management and moved in internal

--- internal/models/user.go
+++ internal/models/user.go
@@ -27,14 +27,24 @@ type UserCreateRequest struct {
 	Email       string `json:"email" binding:"required,email"`
 	Password    string `json:"password" binding:"required,min=8"`
 	DisplayName string `json:"displayName,omitempty"`
-	Role        string `json:"role" binding:"required"` // Should be validated ("patolog", "admin", "viewer")
 	Institution string `json:"institution,omitempty"`
+	Role        string `json:"role,omitempty" `
 }
 
 // UserUpdateRequest is used for updating user profile
 type UserUpdateRequest struct {
-	DisplayName *string `json:"displayName,omitempty"`
-	Role        *string `json:"role,omitempty"`
-	Institution *string `json:"institution,omitempty"`
-	IsActive    *bool   `json:"isActive,omitempty"`
+	DisplayName     *string `json:"displayName,omitempty"`
+	Role            *string `json:"role,omitempty"`
+	Institution     *string `json:"institution,omitempty"`
+	IsActive        *bool   `json:"isActive,omitempty"`
+	IsEmailVerified *bool   `json:"isEmailVerified,omitempty"` // Custom claim to check if email is verified
+}
+
+// TokenClaims represents the essential claims extracted fom a verified authentication token.
+// This is a generic representation, abstracting away provider-specific token structures.
+type TokenClaims struct {
+	UID             string
+	Email           string
+	Role            string //Custom claim that we expect to be present for authorization
+	IsEmailVerified bool   // Custom claim to check if email is verified
 }
