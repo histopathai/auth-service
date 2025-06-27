227a8d1b6db5196e0c89117f4521c5bb8cc87fa7
yasintuncerr <yasiruncerr@gmail.com>
2025-06-27 12:08:01 +0300

    Refactor main function to initialize Firebase app and clients before adapters

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"log/slog"
 	"os"
 
+	firebase "firebase.google.com/go"
 	"github.com/histopathai/auth-service/adapter"
 	"github.com/histopathai/auth-service/config"
+	"github.com/joho/godotenv"
 
 	"github.com/histopathai/auth-service/internal/service"
 	"github.com/histopathai/auth-service/internal/utils"
@@ -14,6 +17,12 @@ import (
 )
 
 func main() {
+
+	// initialize environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Println("⚠️  .env file not loaded, falling back to OS env vars")
+	}
+
 	// Initialize structured logging
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
@@ -26,14 +35,32 @@ func main() {
 		log.Fatalf("Failed to load config : %v", err)
 	}
 
-	// Initialize Firabase  Auth repository
-	authRepo, err := adapter.NewFirebaseAuthAdapter(cfg.Firebase)
+	// Initialize Firebase App with ProjectID
+	app, err := firebase.NewApp(context.Background(), &firebase.Config{
+		ProjectID: cfg.Firebase.ProjectID,
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize Firebase app: %v", err)
+	}
+
+	authClient, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to initialize Firebase Auth client: %v", err)
+	}
+
+	// Initialize Firestore client
+	firestoreClient, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to initialize Firestore client: %v", err)
+	}
+
+	authRepo, err := adapter.NewFirebaseAuthAdapter(authClient)
 	if err != nil {
 		log.Fatalf("Failed to initialize Firebase Auth adapter: %v", err)
 	}
 
-	//Initialize Firestore repository
-	userRepo, err := adapter.NewFirestoreAdapter(cfg.Firestore)
+	// Initialize Firestore repository
+	userRepo, err := adapter.NewFirestoreAdapter(firestoreClient, cfg.Firestore.UsersCollection)
 	if err != nil {
 		log.Fatalf("Failed to initialize Firestore adapter: %v", err)
 	}
