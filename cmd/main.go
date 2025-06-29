fded0f74414af94da810b35b786bda215590f827
yasintuncerr <yasiruncerr@gmail.com>
2025-06-29 14:39:46 +0300

    Enhance API documentation with Swagger annotations for Auth Service

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,23 @@
+// @title HistopathAI Auth Service API
+// @version 1.0
+// @description HistopathAI Authentication Service API documentation.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+// @schemes http
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
 package main
 
 import (
