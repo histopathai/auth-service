db743c10db88b8327bf190291ffd6e76a158aed5
yasintuncer <yasintuncerr@outlook.com>
2025-11-11 23:38:51 +0300

    style: format Terraform configuration for improved readability

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,16 +1,26 @@
 terraform {
   required_version = ">=1.5.0"
-
+  
   required_providers {
     google = {
-        source  = "hashicorp/google"
-        version = "~> 5.0"
+      source  = "hashicorp/google"
+      version = "~> 5.0"
     }
   }
-
-  backend "gcs"  {
+  
+  backend "gcs" {
     bucket = "tf-state-histopathai-platform"
     prefix = "services/auth-service"
-    }
-
+  }
 }
+
+# Platform-infra outputs'unu kullan
+data "terraform_remote_state" "platform" {
+  backend = "gcs"
+  
+  config = {
+    bucket = "tf-state-histopathai-platform"
+    prefix = "platform-infra"
+  }
+}
+
