57b59a6c4a78bbb953ddfdff3ebb1a3b2a8a4412
yasintuncer <yasintuncerr@outlook.com>
2025-11-18 02:03:43 +0300

    refactor: remove Register method from AuthRepository interface

--- internal/domain/repository/auth.go
+++ internal/domain/repository/auth.go
@@ -7,13 +7,11 @@ import (
 )
 
 type AuthRepository interface {
-	Register(ctx context.Context, payload *model.RegisterUser) (*model.UserAuthInfo, error)
-
 	VerifyIDToken(ctx context.Context, idToken string) (*model.UserAuthInfo, error)
 
 	ChangePassword(ctx context.Context, userID string, newPassword string) error
 
 	Delete(ctx context.Context, userID string) error
 
-	GetAuthInfo(ctx context.Context, userID string) (*model.UserAuthInfo, error) // <-- DOĞRU TİP
+	GetAuthInfo(ctx context.Context, userID string) (*model.UserAuthInfo, error)
 }
