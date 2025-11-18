dce29acfbb476288e25b33f7606e993c9fca427b
yasintuncer <yasintuncerr@outlook.com>
2025-11-18 15:25:21 +0300

    refactor: streamline ApplyDefaults method in ListUsersRequest for clarity

--- internal/api/http/dto/request/admin.go
+++ internal/api/http/dto/request/admin.go
@@ -1,24 +1,20 @@
 package request
 
-// ListUsersRequest represents query parameters for listing users
 type ListUsersRequest struct {
 	PaginationRequest
 }
 
-// Default sort field for users
 const DefaultUserSortBy = "created_at"
 
-// ApplyDefaults sets default values for list users request
 func (r *ListUsersRequest) ApplyDefaults() {
-	r.PaginationRequest.ApplyDefaults(DefaultUserSortBy)
+	if r.SortBy == nil {
+		defaultSort := DefaultUserSortBy
+		r.SortBy = &defaultSort
+	}
+
+	r.PaginationRequest.ApplyDefaults()
 }
 
-// GetAllowedSortFields returns allowed sort fields for users
 func (r *ListUsersRequest) GetAllowedSortFields() []string {
 	return []string{"created_at", "updated_at", "email", "display_name"}
-}
-
-// ChangeUserPasswordRequest represents admin password change request
-type ChangeUserPasswordRequest struct {
-	NewPassword string `json:"new_password" binding:"required,min=8" example:"NewStrongP@ss123"`
 }
